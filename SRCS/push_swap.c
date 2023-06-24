634f7a355413684d34226be70f96dc4056e833f9
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2023-06-24 19:41:24 +0200

    save

--- SRCS/push_swap.c
+++ SRCS/push_swap.c
@@ -6,16 +6,12 @@
 /*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 16:34:12 by edelarbr          #+#    #+#             */
-/*   Updated: 2023/05/29 22:10:16 by edelarbr         ###   ########.fr       */
+/*   Updated: 2023/06/24 19:41:12 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// dans freeall mettre if (s) au début des free de s et laisser free(s) à la fin des free s
-
-// gérer ./push_swap "" "" (atoi error doit etre = à 1
-// OK : gérer ./push_swap 000000002 000000000000000000000000000000001 (retirer les 0 avant le parse du strlen > 11
 int	main(int argc, char **argv)
 {
 	t_stack	*s;
